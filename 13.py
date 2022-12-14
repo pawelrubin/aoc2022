bacbdfaa57a6a755eedfea79ce1a76949a6a4096
Paweł Rubin <pawelrubindev@gmail.com>
2022-12-14 18:10:48 +0100

    works

--- 13.py
+++ 13.py
@@ -1,43 +1,70 @@
 from __future__ import annotations
 import json
 import sys
+from enum import Enum, auto
 
 DEBUG = False
-Packet = int | list["Packet"]
+PacketData = int | list["PacketData"]
+
+Packet = list[PacketData]
 
 
-def parse_packet(s: str) -> list[Packet]:
+def parse_packet(s: str) -> Packet:
     return json.loads(s)
 
 
-def right_order(left: list[Packet], right: list[Packet], indent=0) -> bool:
-    for a, b in zip(left, right):
-        if DEBUG:
-            print((indent + 1) * " " + f"- Compare {a} vs {b}")
+class Result(Enum):
+    WRONG = auto()
+    RIGHT = auto()
+    LEFT_EXHAUSTED = auto()
+    EQUAL_LENGTH = auto()
+    RIGHT_EXHAUSTED = auto()
 
-        match a, b:
-            case int(a), int(b):
-                if a == b:
+    @classmethod
+    def from_(cls, boolean: bool) -> Result:
+        return Result.RIGHT if boolean else Result.WRONG
+
+    def __bool__(self) -> bool:
+        return self in (
+            Result.RIGHT,
+            Result.LEFT_EXHAUSTED,
+            Result.EQUAL_LENGTH,
+        )
+
+
+def right_order(left_packet: Packet, right_packet: Packet) -> Result:
+    for left, right in zip(left_packet, right_packet):
+        match left, right:
+            case int(left), int(right):
+                if left == right:
                     continue
 
-                return a < b
+                return Result.from_(left < right)
 
             case [*xs], [*ys]:
-                if right_order(xs, ys, indent + 1):
-                    if len(xs) == len(ys):
+                match right_order(xs, ys):
+                    case Result.RIGHT | Result.LEFT_EXHAUSTED:
+                        return Result.RIGHT
+
+                    case Result.WRONG | Result.RIGHT_EXHAUSTED:
+                        return Result.WRONG
+
+                    case Result.EQUAL_LENGTH:
                         continue
 
-                    return len(xs) < len(ys)
-
-                return False
-
             case int(x), [*ys]:
-                return right_order([x], ys, indent + 1)
+                return right_order([x], ys)
 
             case [*xs], int(y):
-                return right_order(xs, [y], indent + 1)
+                return right_order(xs, [y])
 
-    return len(left) <= len(right)
+    if len(left_packet) < len(right_packet):
+        return Result.LEFT_EXHAUSTED
+
+    if len(left_packet) == len(right_packet):
+        return Result.EQUAL_LENGTH
+
+    return Result.RIGHT_EXHAUSTED
 
 
 with open(sys.argv[1]) as f:
@@ -46,12 +73,20 @@ with open(sys.argv[1]) as f:
 result = 0
 for i, pair in enumerate(contents.split("\n\n"), 1):
     a, b = pair.split("\n")
-    a, b = parse_packet(a), parse_packet(b)
-    if right_order(a, b):
-        if DEBUG:
-            print("\nRIGHT\n")
+    left, right = parse_packet(a), parse_packet(b)
+
+    if right_order(left, right):
         result += i
-    elif DEBUG:
-        print("\nWRONG\n")
 
 print(result)  # 3050 is not the right answer
+
+
+with open(sys.argv[1]) as f:
+    contents = f.read()
+
+packets: list[Packet] = []
+
+
+for pair in contents.split("\n\n"):
+    a, b = pair.split("\n")
+    packets.extend([parse_packet(a), parse_packet(b)])
